00753553714aa7cb6402bcec80270dc011366555
zayed-04 <eng.zayed04@gmail.com>
2018-04-22 22:15:03 +0600

    DropDownViewModel Created

---
+++ Common/Model/DropdownViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Common.Model
+{
+    public class DropdownViewModel
+    {
+        public string Id { get; set; }
+        public string Text { get; set; }
+        public object Data { get; set; }
+    }
+}
