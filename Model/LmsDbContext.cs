f1842987180e1c18c8b037e3ab499ac17f029cb1
zayed-04 <eng.zayed04@gmail.com>
2018-04-22 21:24:07 +0600

    Common Entity And Course and Teacher Model Created as wel as db Migration.

---
+++ Model/LmsDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace Model
+{
+    public class LmsDbContext : DbContext
+    {
+        public LmsDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+    }
+}
---
+++ Model/Teacher.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Model
+{
+    public class Teacher :Entity
+    {
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Courses { get; set; }
+        public double TotalCredit { get; set; }
+    }
+}
