707e121d19160dac10462ee69fe761a0f33ca5fe
zayed-04 <eng.zayed04@gmail.com>
2018-04-23 00:32:33 +0600

    Teacher request model edited.

--- RequestModel/TeacherRequestModel.cs
+++ RequestModel/TeacherRequestModel.cs
@@ -1,19 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq.Expressions;
-using System.Text;
 using Common.Model;
 using Common.RequestModel;
 using Model;
 
 namespace RequestModel
 {
-    public class TeacherRequestModel:BaseRequestModel<Teacher>
+    public class TeacherRequestModel : BaseRequestModel<Teacher>
     {
         public double mincredit { get; set; }
         public double maxcredit { get; set; }
 
-        public TeacherRequestModel(string keyword, string orderBy = "Modified", string isAscending = "False") : base(keyword, orderBy, isAscending)
+        public TeacherRequestModel(string keyword, string orderBy = "Modified", string isAscending = "False") : base(
+            keyword, orderBy, isAscending)
         {
 
         }
@@ -21,7 +20,7 @@ namespace RequestModel
         protected override Expression<Func<Teacher, bool>> GetExpression()
         {
             Keyword = Keyword.ToLower();
-            ExpressionObj = x => x.Name.Contains(Keyword) || x.Phone.Contains(Keyword)||x.Courses.Contains(Keyword);
+            ExpressionObj = x => x.Name.Contains(Keyword) || x.Phone.Contains(Keyword) || x.Courses.Contains(Keyword);
             if (mincredit > 0) ExpressionObj = ExpressionObj.And(x => x.TotalCredit >= mincredit);
             if (maxcredit > 0) ExpressionObj = ExpressionObj.And(x => x.TotalCredit <= maxcredit);
             return ExpressionObj;
@@ -29,7 +28,7 @@ namespace RequestModel
 
         public override Expression<Func<Teacher, DropdownViewModel>> Dropdown()
         {
-            return x => new DropdownViewModel() { Id = x.Id, Text = x.Name, Data = x };
+            return x => new DropdownViewModel() {Id = x.Id, Text = x.Name, Data = x};
         }
     }
 }
