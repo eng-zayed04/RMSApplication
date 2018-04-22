974a31a1544586a549bd7144f8b4bf21452c38ea
zayed-04 <eng.zayed04@gmail.com>
2018-04-23 00:03:19 +0600

    courses cntroller edited

--- RMS.Server/Controllers/CoursesController.cs
+++ RMS.Server/Controllers/CoursesController.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Common.Repository;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Model;
 using RequestModel;
