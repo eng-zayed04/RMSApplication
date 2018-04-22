2802480f98584ed7953ef7da5ba8a7496c953179
zayed-04 <eng.zayed04@gmail.com>
2018-04-23 00:31:16 +0600

    Teacher request model created

---
+++ RequestModel/TeacherRequestModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using Common.Model;
+using Common.RequestModel;
+using Model;
+
+namespace RequestModel
+{
+    public class TeacherRequestModel:BaseRequestModel<Teacher>
+    {
+        public double mincredit { get; set; }
+        public double maxcredit { get; set; }
+
+        public TeacherRequestModel(string keyword, string orderBy = "Modified", string isAscending = "False") : base(keyword, orderBy, isAscending)
+        {
+
+        }
+
+        protected override Expression<Func<Teacher, bool>> GetExpression()
+        {
+            Keyword = Keyword.ToLower();
+            ExpressionObj = x => x.Name.Contains(Keyword) || x.Phone.Contains(Keyword)||x.Courses.Contains(Keyword);
+            if (mincredit > 0) ExpressionObj = ExpressionObj.And(x => x.TotalCredit >= mincredit);
+            if (maxcredit > 0) ExpressionObj = ExpressionObj.And(x => x.TotalCredit <= maxcredit);
+            return ExpressionObj;
+        }
+
+        public override Expression<Func<Teacher, DropdownViewModel>> Dropdown()
+        {
+            return x => new DropdownViewModel() { Id = x.Id, Text = x.Name, Data = x };
+        }
+    }
+}
