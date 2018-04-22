7699062c6111c21d360295ef70e2e01e9504e9f0
zayed-04 <eng.zayed04@gmail.com>
2018-04-22 22:51:50 +0600

    Course and teacher ViewModels created

---
+++ ViewModel/TeacherViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Common.ViewModel;
+using Model;
+
+namespace ViewModel
+{
+    public class TeacherViewModel : BaseViewModel<Teacher>
+    {
+        public TeacherViewModel(Teacher teacher):base(teacher)
+        {
+            Name= teacher.Name;
+            Courses = teacher.Courses;
+            TotalCredit = teacher.TotalCredit;
+        }
+        public string Name { get; set; }
+        public string Courses { get; set; }
+        public double TotalCredit { get; set; }
+    }
+}
