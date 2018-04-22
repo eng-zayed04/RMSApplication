4c3a9276063978389c29e9102ffd74278886757b
zayed-04 <eng.zayed04@gmail.com>
2018-04-23 00:36:33 +0600

    Teacher Service Created

---
+++ Service/TeacherService.cs
@@ -0,0 +1,15 @@
+﻿using Common.Repository;
+using Common.Service;
+using Model;
+using RequestModel;
+using ViewModel;
+
+namespace Service
+{
+    public class TeacherService : BaseService<Teacher, TeacherRequestModel, TeacherViewModel>
+    {
+        public TeacherService(BaseRepository<Teacher> repository) : base(repository)
+        {
+        }
+    }
+}
