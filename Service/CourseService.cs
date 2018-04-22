da52390289977d8d727d43f5461be9cc8ec64b3f
zayed-04 <eng.zayed04@gmail.com>
2018-04-22 23:51:31 +0600

    Course service class added.

---
+++ Service/CourseService.cs
@@ -0,0 +1,15 @@
+﻿using Common.Repository;
+using Common.Service;
+using Model;
+using RequestModel;
+using ViewModel;
+
+namespace Service
+{
+    public class CourseService : BaseService<Course, CourseRequestModel, CourseViewModel>
+    {
+        public CourseService(BaseRepository<Course> repository) : base(repository)
+        {
+        }
+    }
+}
